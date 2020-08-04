93b75d1f2e2d8e6a5ae1a3e03afa8da479655ea0
sebastianlopez <sebalopez2311@gmail.com>
2020-08-04 17:16:54 -0300

    Created local mysql environment config. Addressed MySql DDL issues

--- src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
+++ src/main/java/guru/sfg/beer/inventory/service/domain/BeerInventory.java
@@ -20,8 +20,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.Pattern;
 import java.sql.Timestamp;
 import java.util.UUID;
 
@@ -32,7 +35,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @Entity
-public class BeerInventory extends BaseEntity{
+public class BeerInventory extends BaseEntity {
 
     @Builder
     public BeerInventory(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, UUID beerId,
@@ -43,7 +46,13 @@ public class BeerInventory extends BaseEntity{
         this.quantityOnHand = quantityOnHand;
     }
 
+    @Type(type="org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false )
     private UUID beerId;
+
+    @Column(unique = true, length = 13, columnDefinition = "varchar(13)")
+    @Pattern(regexp = "([0-9]){13}")
     private String upc;
+
     private Integer quantityOnHand = 0;
 }
