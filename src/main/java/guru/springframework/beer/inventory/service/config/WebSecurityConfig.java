1c4e7bcda3af4eb81c7bae1db755b5ce3688cfce
sebastianlopez <sebalopez2311@gmail.com>
2020-08-14 17:14:26 -0300

    Fixed property setting for password. Added missing @Configuration annotation in WebSecurityConfig

--- src/main/java/guru/springframework/beer/inventory/service/config/WebSecurityConfig.java
+++ src/main/java/guru/springframework/beer/inventory/service/config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package guru.springframework.beer.inventory.service.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
